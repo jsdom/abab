b102a2ab3899396ec5b47eb3f3db378769b52a2a
jeffcarp <gcarpenterv@gmail.com>
2015-09-02 22:33:32 +0000

    Add jscs and fix style errors

--- test/abab.js
+++ test/abab.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const btoa = require('..').btoa;
 const atob = require('..').atob;
 
-const plaintext = "Hello, world!";
+const plaintext = 'Hello, world!';
 
 describe('abab', function () {
 
--- test/util.js
+++ test/util.js
@@ -1,7 +1,7 @@
 module.exports = {
   stripChars: function (str) {
     if (typeof str === 'string') {
-      return str.replace(/[^\x20-\x7F]/g, '?'); 
+      return str.replace(/[^\x20-\x7F]/g, '?');
     } else {
       return str;
     }
