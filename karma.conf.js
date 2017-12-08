dbc830341e4b84062dc9831cdc28ceda94846229
Jeff Carpenter <jeffcarp@chromium.org>
2017-12-08 12:53:17 -0800

    Test only Firefox locally in CI, bump nvmrc
    Fixes #21

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,5 @@
 'use strict';
 
-function prefixTag(tagValue, tagPrefix) {
-  return tagValue ? (tagPrefix ? tagPrefix + ':' + tagValue : tagValue) : undefined;
-}
-
-const sauceBrowsers = {
-  'Chrome': {
-    base: 'SauceLabs',
-    browserName: 'Chrome'
-  },
-  'Firefox': {
-    base: 'SauceLabs',
-    browserName: 'Firefox'
-  },
-  'Internet Explorer': {
-    base: 'SauceLabs',
-    browserName: 'Internet Explorer'
-  }
-};
-const browsers = process.env.SAUCE_USERNAME ? Object.keys(sauceBrowsers) : ['Firefox'];
-
-if (!!process.env.SAUCE_USERNAME !== !!process.env.SAUCE_ACCESS_KEY) {
-  throw new Error('Both SAUCE_USERNAME and SAUCE_ACCESS_KEY should be configured');
-}
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -45,28 +21,9 @@ module.exports = function (config) {
 
     frameworks: ['mocha'],
 
-    reporters: ['dots', 'saucelabs'],
+    reporters: ['dots'],
 
-    sauceLabs: {
-      title: process.env.BUILD_NAME,
-      tags:
-        [
-          prefixTag(process.env.TRAVIS_BRANCH, 'branch'),
-          prefixTag(process.env.TRAVIS_PULL_REQUEST, 'pr'),
-          prefixTag(process.env.TRAVIS_COMMIT, 'commit'),
-          prefixTag(process.env.TRAVIS_TAG, 'tag')
-        ]
-        .filter(function (t) {
-          // Remove useless tags
-          return !!t && t !== 'false';
-        }),
-      'public': 'public'
-
-    },
-
-    customLaunchers: process.env.SAUCE_USERNAME && sauceBrowsers,
-
-    browsers: browsers,
+    browsers: ['Firefox'],
     singleRun: true
   });
 };
