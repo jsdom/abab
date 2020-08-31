f22dcf16f2488674b598ebcf56fbefcd3c58a337
Giovanni Frigo <giovanni@belkadigital.com>
2020-09-01 01:38:11 +0200

    Use a lookup string in the atobLookup and btoaLookup functions (#38)
    * Using a lookup string in the atobLookup and btoaLookup functions
    
    * Small changes suggested by the PR reviewer
    
    * adding tests on the keystr used inside atob and btoa
    
    * Removing unreachable character in lookup string

--- test/node.js
+++ test/node.js
@@ -6,6 +6,29 @@ const assert = require("assert");
 const abab = require("..");
 const fixtures = require("./fixtures");
 
+const keystr =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+for (let i = 0; i < 256; i++) {
+  const input = `\0\0${String.fromCharCode(i)}`;
+  const output = abab.btoa(input);
+
+  const expectedOutput = `AA${keystr[Math.floor(i / 64)]}${keystr[i % 64]}`;
+
+  it(`correctly converts ASCII char ${i} into ${expectedOutput}`, () => {
+    assert.strictEqual(expectedOutput, output);
+  });
+}
+
+for (let i = 0; i < 64; i++) {
+  const input = `AAA${keystr[i]}`;
+  const output = abab.atob(input);
+
+  it(`correctly converts ${input} into ${keystr[i]}`, () => {
+    assert.strictEqual(i, output.charCodeAt(2));
+  });
+}
+
 ["atob", "btoa"].forEach(abFnKey => {
   const abFn = abab[abFnKey];
   const cases = fixtures.get(abFnKey);
